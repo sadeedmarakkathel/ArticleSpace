e35262d71320d63615a77fc08ce80c5686ec9b46
sadeedmarakkathel <sadeed.official@gmail.com>
2026-02-26 12:02:45 +0530

    Add leading slashes to static and media URLs

--- core/settings.py
+++ core/settings.py
@@ -101,12 +101,12 @@ USE_I18N = True
 USE_TZ = True
 
 # Static and Media files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cloud Storage (Supabase S3)
